a664a3ff76aca113902ed5dad72cc380412675a9
shahzad akram <alyakram786@gmail.com>
2024-01-22 19:50:09 +0500

    completed desc button

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ let inputprj3 = document.getElementById("input3");
 let inputprj4 = document.getElementById("input4");
 let inputprj5 = document.getElementById("input5");
 let buttonprj1 = document.getElementById("ascbtn1");
-let buttonprj2 = document.getElementById("desbtn1");
+let buttonprj2 = document.getElementById("desbtn2");
 let paraprj1 = document.getElementById("prj1para")
 
 
@@ -46,9 +46,39 @@ buttonprj1.addEventListener('click', () => {
 
     }
     console.log(num);
-    paraprj1.innerHTML += `Here is you Asending order ${num}`
+    paraprj1.innerHTML += `Here is you Asending order: ${num}`
 
 
+
+});
+
+
+// completed asc ordr
+
+
+
+buttonprj2.addEventListener('click', () => {
+    let num2 = [
+        parseFloat(inputprj1.value),
+        parseFloat(inputprj2.value),
+        parseFloat(inputprj3.value),
+        parseFloat(inputprj4.value),
+        parseFloat(inputprj5.value)
+    ]
+    for (let i = 0; i < num2.length; i++) {
+        for (let j = 0; j < num2.length; j++) {
+            if (num2[j] < num2[j + 1]) {
+                let tempAsc = num2[j];
+                num2[j] = num2[j + 1];
+                num2[j + 1] = tempAsc;
+
+            }
+
+        }
+
+    }
+    console.log(num2);
+    paraprj1.innerHTML += `Here is you Desending order: ${num2}`
 
 
 
