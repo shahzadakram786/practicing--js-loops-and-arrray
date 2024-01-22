1f2172dc94f40b105887238a4c04b073c95697a5
shahzad akram <alyakram786@gmail.com>
2024-01-22 18:48:14 +0500

    starting js

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-let num = [2, 3, 52, 1, 4, 36];
-for (let i = 0; i < num.length; i++) {
-    for (let j = 0; j < num.length; j++) {
-        if (num[j] < num[j + 1]) {
-            let temp = num[j]
-            num[j] = num[j + 1]
-            num[j + 1] = temp
+// let num = [2, 3, 52, 1, 4, 36];
+// for (let i = 0; i < num.length; i++) {
+//     for (let j = 0; j < num.length; j++) {
+//         if (num[j] < num[j + 1]) {
+//             let temp = num[j]
+//             num[j] = num[j + 1]
+//             num[j + 1] = temp
+//         }
+//     }
+// }
+// console.log(num);
+
+
+
+let inputprj1 = document.getElementById("input");
+let buttonprj1 = document.getElementById("ascbtn1");
+let buttonprj2 = document.getElementById("desbtn1");
+let paraprj1 = document.getElementById("prj1para")
+
+buttonprj1.addEventListener('click', () => {
+
+    let num = parseInt(inputprj1.value);
+    for (let i = 0; i < num.length; i++) {
+        for (let j = 0; j < num.length; j++) {
+            if (num[j] > num[j + 1]) {
+                let tempAsc = num[j];
+                num[j] = num[j + 1];
+                num[j + 1] = tempAsc;
+
+            }
+
         }
+
     }
-}
-console.log(num);
+    paraprj1.innerHTML = ``
+
+
+
+
+
+})
