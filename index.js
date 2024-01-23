b4ace784b8514aabfd78b6cc7f7ff283c32baf3a
shahzad akram <alyakram786@gmail.com>
2024-01-23 14:33:15 +0500

    adding strings buttons

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ let inputprj4 = document.getElementById("input4");
 let inputprj5 = document.getElementById("input5");
 let buttonprj1 = document.getElementById("ascbtn1");
 let buttonprj2 = document.getElementById("desbtn2");
+let buttonprjS1 = document.getElementById("AscStr")
+let buttonprjS2 = document.getElementById("DesStr")
 let paraprj1 = document.getElementById("prj1para")
 
 
@@ -33,23 +35,28 @@ buttonprj1.addEventListener('click', () => {
             parseFloat(inputprj5.value)
         ]
         // let num = parseInt([inputprj1.value]);
-    for (let i = 0; i < num.length; i++) {
-        for (let j = 0; j < num.length; j++) {
-            if (num[j] > num[j + 1]) {
-                let tempAsc = num[j];
-                num[j] = num[j + 1];
-                num[j + 1] = tempAsc;
+
+    if (num.every(Element => !isNaN(Element))) {
+
+        for (let i = 0; i < num.length; i++) {
+            for (let j = 0; j < num.length; j++) {
+                if (num[j] > num[j + 1]) {
+                    let tempAsc = num[j];
+                    num[j] = num[j + 1];
+                    num[j + 1] = tempAsc;
+
+                }
 
             }
 
         }
+        console.log(num);
+        paraprj1.innerHTML = `Here is you Asending order: ${num}`
+
+    } else if (num.every(element => isNaN(element))) {
+        paraprj1.innerHTML = "you have entered wrong data type "
 
     }
-    console.log(num);
-    paraprj1.innerHTML = `Here is you Asending order: ${num}`
-
-
-
 });
 
 
@@ -78,7 +85,7 @@ buttonprj2.addEventListener('click', () => {
     //     (inputprj4.value),
     //     (inputprj5.value)
     // ]
-    if (!isNaN(num2.every(Element => !isNaN(Element)))) {
+    if (num2.every(Element => !isNaN(Element))) {
         for (let i = 0; i < num2.length; i++) {
             for (let j = 0; j < num2.length; j++) {
                 if (num2[j] < num2[j + 1]) {
@@ -93,9 +100,78 @@ buttonprj2.addEventListener('click', () => {
         }
         console.log(num2);
         paraprj1.innerHTML = `Here is you Desending order: ${num2}`
-    } else if (isNaN(num2.every(Element => isNaN(Element)))) {
-        paraprj1.innerHTML += "you have entered wrong data type "
+    } else if (num2.every(element => isNaN(element))) {
+        paraprj1.innerHTML = "you have entered wrong data type "
 
     }
 
+});
+
+
+
+// statring strings in Asending and Descending order
+
+
+buttonprjS1.addEventListener('click', () => {
+    let str1 = [
+            inputprj1.value,
+            inputprj2.value,
+            inputprj3.value,
+            inputprj4.value,
+            inputprj5.value
+        ]
+        // let nu = parseInt([inputprj1.value]);
+
+    if (str1.every(Element => isNaN(Element))) {
+
+        for (let i = 0; i < str1.length; i++) {
+            for (let j = 0; j < str1.length; j++) {
+                if (str1[j] > str1[j + 1]) {
+                    let tempAscS1 = str1[j];
+                    str1[j] = str1[j + 1];
+                    str1[j + 1] = tempAscS1;
+
+                }
+
+            }
+
+        }
+        console.log(str1);
+        paraprj1.innerHTML = `Here is you Asending order: ${str1}`
+
+    } else if (str1.every(element => !isNaN(element))) {
+        paraprj1.innerHTML = "you have entered wrong data type "
+
+    }
+});
+buttonprjS2.addEventListener('click', () => {
+    let str2 = [
+            inputprj1.value,
+            inputprj2.value,
+            inputprj3.value,
+            inputprj4.value,
+            inputprj5.value
+        ]
+        // let nu = parseInt([inputprj1.value]);
+
+    if (str2.every(Element => isNaN(Element))) {
+
+        for (let i = 0; i < str2.length; i++) {
+            for (let j = 0; j < str2.length; j++) {
+                if (str2[j] < str2[j + 1]) {
+                    let tempAscS2 = str2[j];
+                    str2[j] = str2[j + 1];
+                    str2[j + 1] = tempAscS2;
+
+                }
+            }
+
+        }
+        console.log(str2);
+        paraprj1.innerHTML = `Here is you Asending order: ${str2}`
+
+    } else if (str2.every(element => !isNaN(element))) {
+        paraprj1.innerHTML = "you have entered wrong data type "
+
+    }
 });
